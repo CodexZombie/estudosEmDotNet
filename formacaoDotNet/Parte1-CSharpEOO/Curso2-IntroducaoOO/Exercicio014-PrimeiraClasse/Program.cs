6b91c18ed8139b195daa48c5923f1404c725b471
CodexZombie <walvesdg@gmail.com>
2019-03-21 00:27:55 -0300

    Add Exercicios do segundo curso

---
+++ formacaoDotNet/Parte1-CSharpEOO/Curso2-IntroducaoOO/Exercicio014-PrimeiraClasse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio014_PrimeiraClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaPatricia = new ContaCorrente();
+
+            contaDaPatricia.titular = "Patrícia";
+            contaDaPatricia.agencia = 123;
+            contaDaPatricia.numero = 12345;
+            contaDaPatricia.saldo = 350;
+
+            Console.WriteLine("Titular: " + contaDaPatricia.titular);
+            Console.WriteLine("Agencia: " + contaDaPatricia.agencia);
+            Console.WriteLine("Conta: " + contaDaPatricia.numero);
+            Console.WriteLine("Saldo: " + contaDaPatricia.saldo);
+            
+            contaDaPatricia.saldo += 150;
+            Console.WriteLine("Saldo: " + contaDaPatricia.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
