217f330e323186a05590474e42c6c8d216d1822e
CodexZombie <walvesdg@gmail.com>
2019-03-21 13:24:48 -0300

    Adicionando Exercicio 015

---
+++ formacaoDotNet/Parte1-CSharpEOO/Curso2-IntroducaoOO/Exercicio015-TiposReferenciaEValor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio015_TiposReferenciaEValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tipo de Referencia
+            ContaCorrente contaDaPatricia = new ContaCorrente();
+            contaDaPatricia.titular = "Patricia";
+            contaDaPatricia.agencia = 123;
+            contaDaPatricia.numero = 12345;
+            contaDaPatricia.saldo = 200;
+
+            ContaCorrente contaDaPatricia2 = new ContaCorrente();
+            contaDaPatricia2.titular = "Patricia";
+            contaDaPatricia2.agencia = 123;
+            contaDaPatricia2.numero = 12345;
+            contaDaPatricia2.saldo = 200;
+
+            Console.WriteLine("Igualdade de tipo de Referência: " + (contaDaPatricia == contaDaPatricia2));
+
+            // Tipo de Valor
+            int numeroQualquer = 18;
+            int outroNumero = 18;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (numeroQualquer == outroNumero));
+
+
+            contaDaPatricia = contaDaPatricia2;
+
+            Console.WriteLine("Igualdade de tipo de Referência: " + (contaDaPatricia == contaDaPatricia2));
+
+            Console.WriteLine("Saldo das contas 1 e 2: " + contaDaPatricia.saldo + " " + contaDaPatricia2.saldo);
+
+            contaDaPatricia.saldo = 500;
+
+            Console.WriteLine("Saldo das contas 1 e 2 após alterar o saldo da conta 1: "
+                + contaDaPatricia.saldo + " " + contaDaPatricia2.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
