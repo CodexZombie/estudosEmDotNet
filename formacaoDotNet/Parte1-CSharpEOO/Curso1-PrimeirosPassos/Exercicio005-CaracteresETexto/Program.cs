9036676555eb2e4c3f98e08234584e9212726727
CodexZombie <walvesdg@gmail.com>
2019-03-20 22:42:10 -0300

    Adicionando os exercicios

---
+++ formacaoDotNet/Parte1-CSharpEOO/Curso1-PrimeirosPassos/Exercicio005-CaracteresETexto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio005_CaracteresETexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o exercicio 5 - Caracteres e Texto");
+
+            //Character
+            char letraChar = 'a'; //aspas simples
+            Console.WriteLine("Primeira Letra: " + letraChar);
+
+            letraChar = (char)65;
+            Console.WriteLine("Letra '65': " + letraChar);
+            Console.WriteLine("Letra '65' + 2: " + (char)(letraChar + 2));
+
+            string frase = "Olá, eu sou uma string";
+            Console.WriteLine(frase);
+
+            Console.WriteLine(frase + ". Essa parte está concatenada.");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ formacaoDotNet/Parte1-CSharpEOO/Curso1-PrimeirosPassos/Exercicio007-Condicionais2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio007_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o exercicio 7 - Condicionais 2");
+
+            int idadeJoao = 16;
+            int qtdPessoas = 2;
+
+            if (idadeJoao >= 18 || qtdPessoas >= 2)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
