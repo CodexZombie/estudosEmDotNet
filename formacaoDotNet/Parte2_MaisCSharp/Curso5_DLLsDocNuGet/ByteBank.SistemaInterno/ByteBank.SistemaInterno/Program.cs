d8c2a028702b608d8f79dddc85039f654bbc5865
CodexZombie <walvesdg@gmail.com>
2019-04-07 04:07:20 -0300

    Referências de DLL

---
+++ formacaoDotNet/Parte2_MaisCSharp/Curso5_DLLsDocNuGet/ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,21 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(123, 1234567);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
