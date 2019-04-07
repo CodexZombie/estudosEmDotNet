86d89274bddd835c365fd49955e05c27858843ec
CodexZombie <walvesdg@gmail.com>
2019-04-07 18:52:26 -0300

    Trabalhando com datas

--- formacaoDotNet/Parte2_MaisCSharp/Curso5_DLLsDocNuGet/ByteBank.SistemaAgencia/Program.cs
+++ formacaoDotNet/Parte2_MaisCSharp/Curso5_DLLsDocNuGet/ByteBank.SistemaAgencia/Program.cs
@@ -11,13 +11,33 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            //TestaContaCorrente();
+
+            DateTime dataFimPagto = new DateTime(2019, 08, 17);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagto - dataCorrente;
+            
+
+            Console.WriteLine(dataFimPagto);
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(GetIntervaloDeTempoLegivel(diferenca));
+
+            Console.ReadLine();
+        }
+
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+            return timeSpan.Days + " dias.";
+        }
+
+        static void TestaContaCorrente()
+        {
             ContaCorrente conta = new ContaCorrente(123, 1234567);
 
             new ContaCorrente(123, 456).Sacar(100);
 
             Console.WriteLine(conta.Numero);
-
-            Console.ReadLine();
         }
     }
 }
