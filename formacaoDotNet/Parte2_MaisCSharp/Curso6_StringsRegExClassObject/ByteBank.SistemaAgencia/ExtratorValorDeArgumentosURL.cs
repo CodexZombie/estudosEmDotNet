78cd6e050012336f9fcaf7a43065280315b6d49f
CodexZombie <walvesdg@gmail.com>
2019-04-09 04:26:07 -0300

    Length e sobrecargas de IndexOf

--- formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -8,9 +8,20 @@ namespace ByteBank.SistemaAgencia
 {
     public class ExtratorValorDeArgumentosURL
     {
+        private readonly string _argumentos;
+        public string URL { get; }
+
         public ExtratorValorDeArgumentosURL(string url)
         {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio.", nameof(url));
+            }
 
+            URL = url;
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
         }
     }
 }
--- formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/ByteBank.SistemaAgencia/Program.cs
+++ formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/ByteBank.SistemaAgencia/Program.cs
@@ -14,6 +14,14 @@ namespace ByteBank.SistemaAgencia
         {
             //string url = "pagina?modedaOrigem=real&moedaDestino=dolar";
 
+            //TesteIsNullOrEmpty();
+            TesteLengthESobrecargasDeIndexOf();
+
+            Console.ReadLine();
+        }
+
+        static void TesteIsNullOrEmpty()
+        {
             string textoVazio = "";
             string textoNulo = null;
             string textoQualquer = "asdfasdfasdf";
@@ -21,8 +29,20 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine(string.IsNullOrEmpty(textoVazio));
             Console.WriteLine(string.IsNullOrEmpty(textoNulo));
             Console.WriteLine(string.IsNullOrEmpty(textoQualquer));
+        }
 
-            Console.ReadLine();
+        static void TesteLengthESobrecargasDeIndexOf()
+        {
+            string palavra = "modedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
         }
     }
 }
