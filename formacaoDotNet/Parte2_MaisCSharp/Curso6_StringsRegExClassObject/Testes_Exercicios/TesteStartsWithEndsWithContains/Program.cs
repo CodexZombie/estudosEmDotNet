6000ee0b1b4fdc6e90a6e3533bd7f6d1e002e58b
CodexZombie <walvesdg@gmail.com>
2019-04-11 02:27:26 -0300

    Teste StartsWith(), EndsWith() e Contains().

---
+++ formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/Testes_Exercicios/TesteStartsWithEndsWithContains/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteStartsWithEndsWithContains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlTeste = "https://www.meusite.com/umapagina";
+
+            Console.WriteLine(urlTeste.StartsWith("https://www.meusite.com"));
+            Console.WriteLine(urlTeste.StartsWith("https://www.google.com"));
+
+            Console.WriteLine(urlTeste.Contains("meusite"));
+            Console.WriteLine(urlTeste.Contains("MEUSITE"));
+
+            Console.WriteLine(urlTeste.EndsWith("umapagina"));
+            Console.WriteLine(urlTeste.EndsWith("umapagina/"));
+
+            Console.ReadLine();
+        }
+    }
+}
