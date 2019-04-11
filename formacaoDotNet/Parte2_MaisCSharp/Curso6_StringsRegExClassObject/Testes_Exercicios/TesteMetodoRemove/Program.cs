6aaae33666cdde0e3cea92c8fe90ccb34069de7c
CodexZombie <walvesdg@gmail.com>
2019-04-11 01:38:03 -0300

    metodo Remove()

---
+++ formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/Testes_Exercicios/TesteMetodoRemove/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteMetodoRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testeRemove = "primeiraParte&parteParaRemover";
+            int indiceEComercial = testeRemove.IndexOf('&');
+
+            string aposRemocao = testeRemove.Remove(indiceEComercial);
+            Console.WriteLine(aposRemocao);
+
+            //Erro quando não encontra o valor procurado:
+            string texto = "ABCDE";
+            Console.WriteLine(texto.IndexOf('F'));
+        }
+    }
+}
