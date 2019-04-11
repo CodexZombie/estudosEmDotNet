5174c0bf9f6b20fd32b5af57d7003a30c1acba88
CodexZombie <walvesdg@gmail.com>
2019-04-11 05:33:50 -0300

    RegEx: Quantificadores com Intervalos

--- formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/Testes_Exercicios/Exercicio039_RegularExpression/Program.cs
+++ formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/Testes_Exercicios/Exercicio039_RegularExpression/Program.cs
@@ -11,31 +11,57 @@ namespace Exercicio039_RegularExpression
     {
         static void Main(string[] args)
         {
-            string textoDeTeste = "Olá, meu nome é José e meu telefone é 4675-3041.";
+            /* SIMPLIFICAÇÃO DAS REGEX
+             * Padrão inicial: 
+             * [0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]
+             * [0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]
+             * [0-9]{4}[-][0-9]{4}
+             * [0-9]{4,5}[-]{0,1}[0-9]{4}
+             * [0-9]{4,5}-{0,1}[0-9]{4}
+             * [0-9]{4,5}-?[0-9]{4}
+             *  
+             * */
 
-            string padrao = "[0123456789][0123456789][0123456789][0123456789]" +
-                "[-][0123456789][0123456789][0123456789][0123456789]";
+            string textoDeTeste = "Exemplo 1: Telefone 3572-2080";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            TestaRegex(textoDeTeste, padrao);
+            
+            textoDeTeste = "Exemplo 2: 37781669 é outro numero, em outro lugar e sem hifem.";
+            TestaRegex(textoDeTeste, padrao);
 
-            Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao));
+            textoDeTeste = "Exemplo 3: Número de Celular 99532-2049.";
+            TestaRegex(textoDeTeste, padrao);
 
-            Match resultadoTeste = Regex.Match(textoDeTeste, padrao);
-            Console.WriteLine(resultadoTeste.Value);
+            textoDeTeste = "Exemplo 3: Número de Celular sem hífem 995322080.";
+            TestaRegex(textoDeTeste, padrao);
 
+            textoDeTeste = "Exercício CPF: 123.456.789.10";
+            padrao = "[0-9]{3}.?[0-9]{3}.?[0-9]{3}.?[0-9]{2}";
+            TestaRegex(textoDeTeste, padrao);
 
-            textoDeTeste = "3778-1669 é o novo numero e está em outro lugar.";
-            padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            textoDeTeste = "Exercício CPF: 123.456.789-10";
+            padrao = "[0-9]{3}.?[0-9]{3}.?[0-9]{3}.?[0-9]{2}";
+            TestaRegex(textoDeTeste, padrao);
 
-            Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao));
+            textoDeTeste = "Exercício CPF: 123456789-10";
+            padrao = "[0-9]{3}.?[0-9]{3}.?[0-9]{3}.?[0-9]{2}";
+            TestaRegex(textoDeTeste, padrao);
 
-            resultadoTeste = Regex.Match(textoDeTeste, padrao);
-            Console.WriteLine(resultadoTeste.Value);
-
-            padrao = "[0-9]{4}[-][0-9]{4}";
-            Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao));
-            resultadoTeste = Regex.Match(textoDeTeste, padrao);
-            Console.WriteLine(resultadoTeste.Value);
+            textoDeTeste = "Exercício CPF: 12345678910";
+            padrao = "[0-9]{3}.?[0-9]{3}.?[0-9]{3}.?[0-9]{2}";
+            TestaRegex(textoDeTeste, padrao);
 
             Console.ReadLine();
+        }
+
+        static void TestaRegex(string teste, string regex)
+        {
+            Console.WriteLine("String: " + teste);
+            Console.WriteLine("Regex: " + regex);
+            Console.WriteLine("Regex.IsMatch(): " + Regex.IsMatch(teste, regex));
+            Match resultadoTeste = Regex.Match(teste, regex);
+            Console.WriteLine("Saída para match.Value: " + resultadoTeste.Value);
+            Console.WriteLine();
         }
     }
 }
