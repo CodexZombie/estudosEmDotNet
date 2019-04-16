bc120fb1929dd51506fd74d4db68878b77ec392e
CodexZombie <walvesdg@gmail.com>
2019-04-16 20:28:05 -0300

    Método Equals()

--- formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/ByteBank.Modelos/Cliente.cs
+++ formacaoDotNet/Parte2_MaisCSharp/Curso6_StringsRegExClassObject/ByteBank.Modelos/Cliente.cs
@@ -11,5 +11,18 @@ namespace ByteBank.Modelos
         public string Nome { get; set; }
         public string CPF { get; set; }
         public string Profissao { get; set; }
+
+        public override bool Equals(object obj)
+        {
+
+            Cliente outroCliente = obj as Cliente;
+
+            if (outroCliente == null)
+            {
+                return false;
+            }
+
+            return CPF == outroCliente.CPF;
+        }
     }
 }
