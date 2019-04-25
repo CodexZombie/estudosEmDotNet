9fbb9d28ecac2046a10390fa2d3339f294fa8425
CodexZombie <walvesdg@gmail.com>
2019-04-25 17:46:47 -0300

    Indexadores

--- formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBank/ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBank/ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -11,6 +11,14 @@ namespace ByteBank.SistemaAgencia
     {
         private ContaCorrente[] _itens;
         private int _proximaPosicao;
+        public int Tamanho
+        {
+            get
+            {
+                return _proximaPosicao;
+            }
+        }
+
 
         public ListaDeContaCorrente(int capacidadeInicial = 5)
         {
@@ -72,6 +80,24 @@ namespace ByteBank.SistemaAgencia
 
             _proximaPosicao--;
             _itens[_proximaPosicao] = null;
+        }
+
+        public ContaCorrente GetItemNoIndice(int indice)
+        {
+            if(indice < 0 || indice >= _proximaPosicao)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return _itens[indice];
+        }
+
+        public ContaCorrente this[int indice]
+        {
+            get
+            {
+                return GetItemNoIndice(indice);
+            }
         }
 
         public void ListarContas()
--- formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -12,8 +12,29 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+
+            ListaDeContaCorrente lista = TesteCriaListaDeContas();
+            TesteMetodoAdicionar(lista);
+            TesteMetodoRemover(lista);
+
+
+            for (int i = 0; i < lista.Tamanho; i++)
+            {
+                ContaCorrente itemAtual = lista[i];
+                Console.WriteLine($"Item na posição {i} Ag/CC: {itemAtual.Agencia}/{itemAtual.Numero}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static ListaDeContaCorrente TesteCriaListaDeContas()
+        {
             ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            return lista;
+        }
 
+        static void TesteMetodoAdicionar(ListaDeContaCorrente lista)
+        {
             lista.Adicionar(new ContaCorrente(123, 1234567));
             lista.Adicionar(new ContaCorrente(123, 1234567));
             lista.Adicionar(new ContaCorrente(123, 1234567));
@@ -25,24 +46,22 @@ namespace ByteBank.SistemaAgencia
             lista.Adicionar(new ContaCorrente(123, 1234567));
             lista.Adicionar(new ContaCorrente(123, 1234567));
 
+        }
+
+        static void TesteMetodoRemover(ListaDeContaCorrente lista)
+        {
             ContaCorrente contaTesteRemocao = new ContaCorrente(999, 9999999);
-
             lista.Adicionar(contaTesteRemocao);
             lista.Adicionar(new ContaCorrente(123, 1234567));
             lista.Adicionar(new ContaCorrente(123, 1234567));
             lista.Adicionar(new ContaCorrente(123, 1234567));
             lista.Adicionar(new ContaCorrente(123, 1234567));
             Console.WriteLine();
-
-
             lista.ListarContas();
-
             lista.Remover(contaTesteRemocao);
-
             Console.WriteLine();
             lista.ListarContas();
-
-            Console.ReadLine();
+            Console.WriteLine();
         }
     }
 }
