d7fb523e18308ac240ec6b8858d868ad44debb61
CodexZombie <walvesdg@gmail.com>
2019-04-28 22:32:52 -0300

    Tipos Genéricos

---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBank/ByteBank.SistemaAgencia/Lista.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class Lista<T>
+    {
+        private T[] _itens;
+        private int _proximaPosicao;
+
+        public int Tamanho
+        {
+            get
+            {
+                return _proximaPosicao;
+            }
+        }
+
+
+        public Lista(int capacidadeInicial = 5)
+        {
+            _itens = new T[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+
+        public void Adicionar(T item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+        public void AdicionarVarios(params T[] itens)
+        {
+            foreach (T item in itens)
+            {
+                Adicionar(item);
+            }
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            T[] novoArray = new T[novoTamanho];
+
+            for (int indice = 0; indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+            }
+
+            _itens = novoArray;
+        }
+
+        public void Remover(T item)
+        {
+            int indiceItem = -1;
+
+            for (int i = 0; i < _proximaPosicao; i++)
+            {
+                T itemAtual = _itens[i];
+                if (itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+
+            for (int i = indiceItem; i < _proximaPosicao; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximaPosicao--;
+            _itens[_proximaPosicao] = null;
+        }
+
+        public T GetItemNoIndice(int indice)
+        {
+            if (indice < 0 || indice >= _proximaPosicao)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return _itens[indice];
+        }
+
+        public T this[int indice]
+        {
+            get
+            {
+                return GetItemNoIndice(indice);
+            }
+        }
+
+    }
+}
--- formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -12,21 +12,22 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            //ListaDeContaCorrente lista = TesteCriaListaDeContas();
+            //TesteMetodoAdicionar(lista);
+            //TesteMetodoRemover(lista);
+            //TesteIndexador(lista);
+            //TesteAdicionarVarios(lista);
+            //lista.ListarContas();
+            //lista.TestaListaDeObject();
 
-            ListaDeContaCorrente lista = TesteCriaListaDeContas();
-            TesteMetodoAdicionar(lista);
-            TesteMetodoRemover(lista);
-            TesteIndexador(lista);
+            Lista<int> inteiros = new Lista<int>();
+            inteiros.AdicionarVarios(1, 2, 3, 4, 5);
+            inteiros.Adicionar("Amarelo");
 
-            lista.AdicionarVarios(
-                new ContaCorrente(999, 8888888),
-                new ContaCorrente(999, 8888888),
-                new ContaCorrente(999, 8888888),
-                new ContaCorrente(999, 8888888),
-                new ContaCorrente(999, 8888888)
-                );
+            Lista<string> cores = new Lista<string>();
+            cores.AdicionarVarios("Cyan", "Magenta", "Yellow", "Black");
+            cores.Adicionar(10);
 
-            lista.ListarContas();
 
             Console.ReadLine();
         }
@@ -76,6 +77,33 @@ namespace ByteBank.SistemaAgencia
                 Console.WriteLine($"Item na posição {i} Ag/CC: {itemAtual.Agencia}/{itemAtual.Numero}");
             }
             Console.WriteLine();
+        }
+
+        static void TesteAdicionarVarios(ListaDeContaCorrente lista)
+        {
+            lista.AdicionarVarios(
+                new ContaCorrente(999, 8888888),
+                new ContaCorrente(999, 8888888),
+                new ContaCorrente(999, 8888888),
+                new ContaCorrente(999, 8888888),
+                new ContaCorrente(999, 8888888)
+                );
+        }
+
+        static void TesteListaDeObject()
+        {
+            ListaDeObject listaDeIdades = new ListaDeObject();
+            listaDeIdades.Adicionar(10);
+            listaDeIdades.Adicionar(5);
+            listaDeIdades.Adicionar(4);
+            listaDeIdades.Adicionar(8);
+            listaDeIdades.AdicionarVarios(25, 13, 15, 14, 16, 17, 7, 6, 5, 4);
+
+            for (int i = 0; i < listaDeIdades.Tamanho; i++)
+            {
+                int idade = (int)listaDeIdades[i];
+                Console.WriteLine($"Idade no indice {i}: {idade}");
+            }
         }
 
     }
