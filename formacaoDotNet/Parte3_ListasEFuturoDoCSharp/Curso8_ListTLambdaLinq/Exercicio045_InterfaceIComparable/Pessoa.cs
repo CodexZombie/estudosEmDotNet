4006c7061cc80c702b866ee8ce91933a96887c03
CodexZombie <walvesdg@gmail.com>
2019-05-08 21:22:01 -0300

    Implementando a interface IComparable

---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/Curso8_ListTLambdaLinq/Exercicio045_InterfaceIComparable/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio045_InterfaceIComparable
+{
+    public class Pessoa : IComparable
+    {
+        public string Nome { get; }
+        public int Idade { get; }
+
+        public Pessoa (string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        // Retornar negativo quando a instância precede o obj;
+        // Retornar zero quando a instância e obj forem equivalentes;
+        // Retornar positivo diferente de zero quando a precedência for de obj;
+        public int CompareTo(object obj)
+        {
+            var p2 = obj as Pessoa;
+            return String.Compare(Nome, p2.Nome);
+        }
+    }
+}
---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/Curso8_ListTLambdaLinq/Exercicio045_InterfaceIComparable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio045_InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoas = new List<Pessoa>()
+            {
+                new Pessoa("Bianca", 35),
+                new Pessoa("Katia", 24),
+                new Pessoa("Wilson", 18),
+                new Pessoa("Pedro", 17),
+                new Pessoa("Zélio", 23),
+                new Pessoa("Selma", 40),
+                new Pessoa("Helena", 41),
+                new Pessoa("Maria", 43),
+                new Pessoa("Auguto", 45),
+                new Pessoa("José", 42)
+            };
+
+            foreach (var p in pessoas)
+            {
+                Console.WriteLine($"{p.Nome} {p.Idade}");
+            }
+
+            Console.WriteLine("Ordenando...");
+            Console.WriteLine("LISTA ORDENADA:");
+            Console.WriteLine();
+
+            pessoas.Sort();
+
+            foreach (var p in pessoas)
+            {
+                Console.WriteLine($"{p.Nome} {p.Idade}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
