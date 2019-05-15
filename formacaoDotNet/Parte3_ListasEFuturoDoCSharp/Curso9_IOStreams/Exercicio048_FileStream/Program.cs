91807073e278622c5dcd985a0b8c1827e86c0e7e
CodexZombie <walvesdg@gmail.com>
2019-05-15 15:38:09 -0300

    Organizando os exercicios 048 e 049

---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/Curso9_IOStreams/Exercicio048_FileStream/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio048_FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCICIO 048 - FILESTREAM
+            
+            var arquivoURL = "../../../contas.txt";
+            var fluxoDoArquivo = new FileStream(arquivoURL, FileMode.Open);
+            var buffer = new byte[1024]; // 1 kb
+            var bytesLidos = -1;
+
+            fluxoDoArquivo.Read(buffer, 0, 1024);
+
+            while (bytesLidos != 0)
+            {
+                bytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var b in buffer)
+            {
+                Console.Write(b);
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/Curso9_IOStreams/Exercicio049_Encoding/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio049_Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCICIO 049 - ENCODING
+
+            var arquivoURL = "../../../contas.txt";
+            var fluxoDoArquivo = new FileStream(arquivoURL, FileMode.Open);
+            var buffer = new byte[1024]; // 1 kb
+            var bytesLidos = -1;
+
+            fluxoDoArquivo.Read(buffer, 0, 1024);
+
+            while (bytesLidos != 0)
+            {
+                bytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var encoding = new UTF8Encoding();
+            var texto = encoding.GetString(buffer);
+            Console.Write(texto);
+        }
+    }
+}
