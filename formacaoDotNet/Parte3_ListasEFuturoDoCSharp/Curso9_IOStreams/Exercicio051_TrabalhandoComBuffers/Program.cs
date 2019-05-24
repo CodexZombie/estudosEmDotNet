cd05e2bee7c7b37da149828e5605baedac27a055
CodexZombie <walvesdg@gmail.com>
2019-05-24 10:35:04 -0300

    Trabalhando com buffers

---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/Curso9_IOStreams/Exercicio051_TrabalhandoComBuffers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio051_TrabalhandoComBuffers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCICIO 051 - TRABALHANDO COM BUFFERS
+
+            var arquivoURL = "../../../contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(arquivoURL, FileMode.Open))
+            {
+                var buffer = new byte[1024]; // 1 kb
+                var bytesLidos = -1;
+
+                while (bytesLidos != 0)
+                {
+                    bytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+
+                    if(bytesLidos != 0)
+                    {
+                        EscreverBuffer(buffer, bytesLidos);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var encoding = new UTF8Encoding();
+            var texto = encoding.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+        }
+    }
+}
