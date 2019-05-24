4dcad5a76d3b3e885f329fcf8af8ae2c6c985805
CodexZombie <walvesdg@gmail.com>
2019-05-24 11:29:02 -0300

    Convertendo o texto para o tipo ContaCorrente

--- formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Program.cs
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,11 +20,36 @@ namespace ByteBankImportacaoExportacao
                 while (!leitor.EndOfStream)
                 {
                     var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
+                    var conta = ConverterStringParaContaCorrente(linha);
+                    var msg = $"Ag.: {conta.Agencia} C/C: {conta.Numero} Saldo: R$ {conta.Saldo} Titular: {conta.Titular.Nome}";
+                    Console.WriteLine(msg);
                 }
             }
-
+            
             Console.ReadLine();
+        }
+
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            var campos = linha.Split(' ');
+
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2];
+            var nomeTitular = campos[3];
+
+            var agenciaInt = int.Parse(agencia);
+            var numeroInt = int.Parse(numero);
+            var saldoDouble = double.Parse(saldo);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agenciaInt, numeroInt);
+            resultado.Depositar(saldoDouble);
+            resultado.Titular = titular;
+
+            return resultado;
         }
     }
 }
