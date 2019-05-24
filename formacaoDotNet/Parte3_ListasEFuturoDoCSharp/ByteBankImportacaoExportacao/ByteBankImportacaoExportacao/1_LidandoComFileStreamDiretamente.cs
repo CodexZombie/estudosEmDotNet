03fe78581358b92303d8069c97dfe622b8e90399
CodexZombie <walvesdg@gmail.com>
2019-05-24 10:53:05 -0300

    Usando StreamReader e recurso partial

---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+            var enderecoDoArquivo = "../../../../Curso9_IOStreams/contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024];
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    //Console.WriteLine($"\nBytes lidos: {numeroDeBytesLidos}\n");
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = Encoding.UTF8;
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+        }
+    }
+
+}
---
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var arquivoURL = "../../../../Curso9_IOStreams/contas.txt";
+
+            using(var fluxoDoArquivo = new FileStream(arquivoURL, FileMode.Open))
+            using(var leitor = new StreamReader(fluxoDoArquivo))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
