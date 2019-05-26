f57e91f32fd569cad39f3ee6ad471e50b6a1c018
CodexZombie <walvesdg@gmail.com>
2019-05-26 17:14:29 -0300

    Escrita e Leitura Binaria

--- formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Program.cs
+++ formacaoDotNet/Parte3_ListasEFuturoDoCSharp/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Program.cs
@@ -12,7 +12,8 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
-            TestaEscrita();
+            LeituraBinaria();
+
             Console.WriteLine("Aplicação finalizada...");
             Console.ReadLine();
         }
