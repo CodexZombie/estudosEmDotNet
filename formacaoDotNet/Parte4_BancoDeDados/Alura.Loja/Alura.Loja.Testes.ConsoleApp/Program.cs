7eee6d1d651c007b7ac1b0efe680ec96e86fa302
CodexZombie <walvesdg@gmail.com>
2019-06-12 21:15:23 -0300

    ChangeTracker - Unchanged / Modified

--- formacaoDotNet/Parte4_BancoDeDados/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ formacaoDotNet/Parte4_BancoDeDados/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,80 +10,121 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //GravarUsandoAdoNet();
-            //GravarUsandoEntity();
-            //RecuperarProdutos();
-            //ExcluirProdutos();
-            //RecuperarProdutos();
-            AtualizarProduto();
+            using (var contexto = new LojaContext())
+            {
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
 
+                Console.WriteLine("=====================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+
+                var p1 = produtos.Last();
+                p1.Nome = "007 - O Espião Que Me Amava";
+
+                Console.WriteLine("=====================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+
+                //var p1 = produtos.First();
+                //p1.Nome = "Harry Potter";
+                //
+                //contexto.SaveChanges();
+
+                //produtos = contexto.Produtos.ToList();
+                //
+                //foreach (var p in produtos)
+                //{
+                //    Console.WriteLine(p);
+                //}
+            }
 
             Console.ReadLine();
         }
 
-        private static void AtualizarProduto()
-        {
-            GravarUsandoEntity();
-            RecuperarProdutos();
-
-            using (var repo = new ProdutoDAOEntity())
-            {
-                Produto primeiro = repo.Produtos().First();
-                primeiro.Nome = "Cassino Royale - Editado";
-                repo.Atualizar(primeiro);
-            }
-            RecuperarProdutos();
-        }
-
-        private static void ExcluirProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach (var item in produtos)
-                {
-                    repo.Remover(item);
-                }
-            }
-        }
-
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram Encontrados {0} produto(s).", produtos.Count);
-                foreach (var item in produtos)
-                {
-                    Console.WriteLine(item.Nome);
-                }
-            }
-        }
-
-        static void GravarUsandoEntity()
-        {
-            Produto p = new Produto();
-            p.Nome = "Cassino Royale";
-            p.Categoria = "Filmes";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAOEntity())
-            {
-                repo.Adicionar(p);
-            }
-        }
-
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }
+        //static void Main(string[] args)
+        //{
+        //    //GravarUsandoAdoNet();
+        //    //GravarUsandoEntity();
+        //    //RecuperarProdutos();
+        //    //ExcluirProdutos();
+        //    //RecuperarProdutos();
+        //    //AtualizarProduto();
+        //
+        //
+        //    Console.ReadLine();
+        //}
+        //
+        //private static void AtualizarProduto()
+        //{
+        //    GravarUsandoEntity();
+        //    RecuperarProdutos();
+        //
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        Produto primeiro = repo.Produtos().First();
+        //        primeiro.Nome = "Cassino Royale - Editado";
+        //        repo.Atualizar(primeiro);
+        //    }
+        //    RecuperarProdutos();
+        //}
+        //
+        //private static void ExcluirProdutos()
+        //{
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        IList<Produto> produtos = repo.Produtos();
+        //        foreach (var item in produtos)
+        //        {
+        //            repo.Remover(item);
+        //        }
+        //    }
+        //}
+        //
+        //private static void RecuperarProdutos()
+        //{
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        IList<Produto> produtos = repo.Produtos();
+        //        Console.WriteLine("Foram Encontrados {0} produto(s).", produtos.Count);
+        //        foreach (var item in produtos)
+        //        {
+        //            Console.WriteLine(item.Nome);
+        //        }
+        //    }
+        //}
+        //
+        //static void GravarUsandoEntity()
+        //{
+        //    Produto p = new Produto();
+        //    p.Nome = "Cassino Royale";
+        //    p.Categoria = "Filmes";
+        //    p.Preco = 19.89;
+        //
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        repo.Adicionar(p);
+        //    }
+        //}
+        //
+        //private static void GravarUsandoAdoNet()
+        //{
+        //    Produto p = new Produto();
+        //    p.Nome = "Harry Potter e a Ordem da Fênix";
+        //    p.Categoria = "Livros";
+        //    p.Preco = 19.89;
+        //
+        //    using (var repo = new ProdutoDAO())
+        //    {
+        //        repo.Adicionar(p);
+        //    }
+        //}
     }
 }
