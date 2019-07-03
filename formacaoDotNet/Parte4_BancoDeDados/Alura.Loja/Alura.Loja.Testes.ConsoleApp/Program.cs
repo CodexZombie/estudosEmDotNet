2633dc323f2acd17b832e0b4eaab91bd68f34a08
CodexZombie <walvesdg@gmail.com>
2019-07-03 18:06:55 -0300

    OnDelete Cascade

--- formacaoDotNet/Parte4_BancoDeDados/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ formacaoDotNet/Parte4_BancoDeDados/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,19 +14,29 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
+            var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
+            var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
+
             var promocaoDePascoa = new Promocao();
             promocaoDePascoa.Descricao = "Páscoa Feliz";
             promocaoDePascoa.DataInicio = DateTime.Now;
             promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
-            //promocaoDePascoa.Produtos.Add(new Produto());
-            //promocaoDePascoa.Produtos.Add(new Produto());
-            //promocaoDePascoa.Produtos.Add(new Produto());
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
 
             using(var contexto = new LojaContext())
             {
                 var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
                 var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
                 loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                //contexto.Promocoes.Add(promocaoDePascoa);
+                var promocao = contexto.Promocoes.Find(1);
+                contexto.Promocoes.Remove(promocao);
+                ExibeEntries(contexto.ChangeTracker.Entries());
+                contexto.SaveChanges();
             }
 
             Console.ReadLine();
